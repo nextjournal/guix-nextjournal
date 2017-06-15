8b2f81f27a79f44c4c509f811de0dc9255e087c5
Joshua Sierles <joshua@diluvia.net>
2017-06-15 21:03:59 +0200

    automate the process of a guix build, modifying its result for usage with nextjournal, and pushing

--- r-docker.sh
+++ r-docker.sh
@@ -1,26 +1,10 @@
-export PACKAGES="coreutils sed vim bash gcc-toolchain gfortran make \
+export PACKAGES="coreutils gzip sed vim bash linux-libre-headers gcc-toolchain gfortran make \
                    r-minimal r-base64enc r-jsonlite r-plotly r-feather r-devtools"
 
-# result of this line contains the tar path
-# docker exec -e GUIX_PACKAGE_PATH=/modules -t guix guix pack -S /bin=bin -f docker
-
-# export TAR_PATH/home/core/gnu/store/4dxgzhgis7bhycb2kbb4hi012fx9ib9r-docker-pack.tar.gz
-
-# result of this line contains the profile hash
-# sudo docker load < $TAR_PATH
-
-# TODO: extract tar hash and profile hash and set it automatically
-
-export PROFILE_HASH=0c07kaf4x8jmayh1cmzpc4ls9sh4g0ws
-
-docker run --name guix-$PROFILE_HASH -t -d profile:$PROFILE_HASH sh
-docker exec guix-$PROFILE_HASH sh -c "echo R_LIBS=/gnu/store/$PROFILE_HASH-profile/site-library > .Renviron"
-docker exec guix-$PROFILE_HASH sh -c "echo options(repos = c(CRAN = \"http://cran.rstudio.com\"), unzip = 'internal') > .Rprofile"
-
-docker exec guix-$PROFILE_HASH mkdir /tmp
-DOCKER_IMAGE_HASH=`docker commit guix-$PROFILE_HASH`
+export TAR_PATH=`docker exec -e GUIX_PACKAGE_PATH=/modules -t guix guix pack -S /bin=bin -f docker $PACKAGES | grep pack | cut -d/ -f 4`
+export PROFILE_HASH=`sudo docker load < /home/core/gnu/store/${TAR_PATH%?} | cut -d" " -f 3`
 DOCKER_IMAGE=nextjournal/r-guix:$PROFILE_HASH
+docker build -t $DOCKER_IMAGE -f r.dockerfile
 
-docker tag $DOCKER_IMAGE_HASH $DOCKER_IMAGE
 echo "Pushing to $DOCKER_IMAGE"
 docker push $DOCKER_IMAGE
--- r.dockerfile
+++ r.dockerfile
@@ -1,11 +1,10 @@
 ARG PROFILE_HASH
 FROM profile:$PROFILE_HASH
 MAINTAINER Joshua Sierles <joshua@joshua.si>
-ARG PROFILE_HASH
 ENV PROFILE_HASH $PROFILE_HASH
 ENV PATH /root/.guix-profile/bin:$PATH
-RUN echo $PROFILE_HASH
+RUN mkdir /tmp
 RUN echo "options(repos = c(CRAN = 'http://cran.rstudio.com'), unzip = 'internal')" > .Rprofile
 RUN ln -s /gnu/store/${PROFILE_HASH}-profile/etc/profile ~/.profile
-RUN echo 'sh -l -c $@' > run.sh
-RUN mkdir /tmp
+COPY R.sh R.sh
+RUN chmod 755 R.sh
