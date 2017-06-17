e60d513e615fcf916698129438f3a2ce4872b37a
Joshua Sierles <joshua@diluvia.net>
2017-06-17 07:58:09 +0200

    clearer base image with libpeg and libpng needed for some R deps

--- base-image/build.sh
+++ base-image/build.sh
@@ -1,10 +1,20 @@
-export PACKAGES="coreutils grep gzip sed vim bash linux-libre-headers gcc-toolchain gfortran make \
-                   r-minimal r-base64enc r-jsonlite r-plotly r-feather r-devtools"
+PACKAGES="coreutils gawk grep gzip sed vim bash linux-libre-headers gcc-toolchain gfortran make \
+          libjpeg libpng \
+          r-minimal r-base64enc r-jsonlite r-plotly r-feather r-devtools"
 
-export TAR_PATH=`docker exec -e GUIX_PACKAGE_PATH=/modules -t guix guix pack -S /bin=bin -f docker $PACKAGES | grep pack | cut -d/ -f 4`
-export PROFILE_HASH=`sudo docker load < /home/core/gnu/store/${TAR_PATH%?} | cut -d" " -f 3`
+GUIX_COMMAND="guix pack -S /bin=bin -f docker $PACKAGES"
+
+echo $GUIX_COMMAND
+TAR_PATH=/home/core/gnu/store/`docker exec -e GUIX_PACKAGE_PATH=/modules -t guix $GUIX_COMMAND grep pack | grep -v drv | cut -d/ -f 4`
+TAR_PATH=${TAR_PATH%?}
+echo "Loading build from $TAR_PATH"
+
+PROFILE_HASH=`sudo docker load < $TAR_PATH | cut -d" " -f 3 | cut -d: -f 2`
 DOCKER_IMAGE=nextjournal/guix-base:$PROFILE_HASH
-docker build -t $DOCKER_IMAGE --build-arg PROFILE_HASH=$PROFILE_HASH .
 
-echo "Pushing to $DOCKER_IMAGE"
+echo "Building $DOCKER_IMAGE ..."
+
+docker build --build-arg PROFILE_HASH=$PROFILE_HASH -t $DOCKER_IMAGE .
+
+echo "Pushing $DOCKER_IMAGE ..."
 docker push $DOCKER_IMAGE
