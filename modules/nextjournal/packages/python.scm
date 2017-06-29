d130f0a1289056ee35e7baec584aad5f766e94a9
Joshua Sierles <joshua@diluvia.net>
2017-06-29 22:34:58 +0200

    feather requires python arrow

--- modules/nextjournal/packages/python.scm
+++ modules/nextjournal/packages/python.scm
@@ -21,7 +21,11 @@
             "1adivm5w5ji4qv7hq7942vqlk8l2wgw87bdlsia771z14z3zp857"))))
     (build-system python-build-system)
     (propagated-inputs
-      `(("python-pandas>=0.17.0" ,python-pandas)))
+      `(("python-pandas>=0.17.0" ,python-pandas),
+        ("python-arrow", python-arrow)))
+
+
+
     (home-page "http://github.com/wesm/feather")
     (synopsis
       "Simple wrapper library to the Apache Arrow-based Feather File Format")
