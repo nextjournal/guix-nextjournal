9564ae175864ada0091c28e086070f0afe907a74
Joshua Sierles <joshua@diluvia.net>
2017-06-29 22:30:18 +0200

    copy instead of moving bootstrapped store

--- guix-core/init-store.sh
+++ guix-core/init-store.sh
@@ -5,8 +5,8 @@ tar xJf /root/.guix-binary-0.13.0.x86_64-linux.tar.xz
 
 echo "Copying modules to /gnu/store..."
 rm -rf /gnu/store/*
-mv gnu/store/* /gnu/store
+cp -pr gnu/store/* /gnu/store
 
 echo "Copying GUIX database to /var/guix/db..."
 rm -rf /var/guix/db/*
-mv var/guix/db/* /var/guix/db
+cp -pr var/guix/* /var/guix
